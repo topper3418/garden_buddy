49a42781aec17517b5975bae1116eaef071bbdf1
topper3418 <travisopperud@gmail.com>
2026-04-18 19:26:14 -0500

    updated main for more flexible deployment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -19,7 +19,7 @@ createRoot(document.getElementById('root')!).render(
         },
       }}
     >
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </ConfigProvider>
